b3aae2f9143c5f5ca69462948c93045479b68606
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-11-16 15:57:51 +0000

    Create Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Recipe(models.Model):
+    """
+    Recipe Model
+    """
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cocktail")
+    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image_alt = models.CharField(max_length=100, null=False, blank=False, default='green-lime-cocktail')
+    serving = models.IntegerField()
+    time = models.IntegerField()
+    description = models.TextField(blank=False)
+    updated_on = models.DateTimeField(auto_now=True)
+    ingredients = models.TextField(blank=False)
+    directions = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='cocktail_like', blank=True)
+
+    class Meta:
+        """
+        Displays newest created-on cocktails
+        """
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.name
+
+    def number_of_likes(self):
+        """
+        Displays total number of likes
+        """
+        return self.likes.count()
