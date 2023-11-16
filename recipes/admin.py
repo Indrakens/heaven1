c1def5338474186e4604d9daadd0b5dcfbef9802
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-11-16 16:43:13 +0000

    Create recipe admin and comment admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Recipe, Comment
 
-# Register your models here.
+
+@admin.register(Recipe)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'status', 'created_on')
+    search_fields = ['name', 'ingredients']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('name',)} 
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'cocktail', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)     
