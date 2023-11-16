3ce54a1da826102063e7011f43ba973299462250
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-11-16 17:22:44 +0000

    create base.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+    """
+    Displays header and most recent added cocktails
+    """
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
